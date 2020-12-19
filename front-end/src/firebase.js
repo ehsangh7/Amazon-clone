225b4963a7f72fb308bc915bcafda8a833edb6be
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-20 01:45:06 +0330

    create fierbase.js

---
+++ front-end/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAgi3_1tTpvOIlE16Vp1YYTPLovKNQ1uKY",
+    authDomain: "clone-bb962.firebaseapp.com",
+    projectId: "clone-bb962",
+    storageBucket: "clone-bb962.appspot.com",
+    messagingSenderId: "687291233545",
+    appId: "1:687291233545:web:937c452798871f45770fd8",
+    measurementId: "G-1E92SPM8QK"
+  };
