ab089407e8da5a182c6cb646dd6f94eded6cf07d
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-21 17:43:21 +0330

    adding components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="app">
-      <h1>Amazon Creation</h1>
       {/* Header */}
-      
+      <Header />
       {/* Home */}
-
+      <Home />
     </div>
   );
 }
