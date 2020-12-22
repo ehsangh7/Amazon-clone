57261b1eabf26e8e16b7e998838a68b3a8d4c96f
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-23 00:20:23 +0330

    add to checkout page

--- front-end/src/components/Checkout.js
+++ front-end/src/components/Checkout.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import '../styles/Checkout.css'
+import CheckoutProduct from './CheckoutProduct'
+import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 const Checkout = () => {
+    const [{basket}, dispatch] = useStateValue()
     return (
         <div className="checkout">
             <div className="checkout__left">
@@ -12,6 +15,16 @@ const Checkout = () => {
                     <h2 className="checkout__titleH2">
                         Shopping Basket
                     </h2>
+                    {/* map through basket items */}
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                            id={item.id} 
+                            title={item.title}
+                            image={item.image}
+                            price={item.price}
+                            rating={item.rating}/>
+                    ))}
+                    
                 </div>
             </div>
             <div className="checkout__right">
