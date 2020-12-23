162b689c85a0bd4f0609ca0d3d577e0479abbb3e
ehsangh7 <ehsanghochagh@gmail.com>
2020-12-23 18:25:40 +0330

    add user to checkout page

--- front-end/src/components/Checkout.js
+++ front-end/src/components/Checkout.js
@@ -5,13 +5,14 @@ import { useStateValue } from './StateProvider'
 import Subtotal from './Subtotal'
 
 const Checkout = () => {
-    const [{basket}, dispatch] = useStateValue()
+    const [{basket, user}, dispatch] = useStateValue()
     return (
         <div className="checkout">
             <div className="checkout__left">
                 <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""/>
             
                 <div className="checkout__title">
+                    <h3>Hello {user? user.email : 'Guest'}</h3>
                     <h2 className="checkout__titleH2">
                         Shopping Basket
                     </h2>
